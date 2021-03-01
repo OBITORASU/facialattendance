9cd32b611f441a1b47495c13d8f688bacc41c857
Souhit Dey <suprememightyzeus@gmail.com>
2021-03-01 09:05:18 +0530

    fix merge issue

--- webcam.py
+++ webcam.py
@@ -126,52 +126,8 @@ def main():
             print("[-] Terminating!")
             break
 
-#Video capture through webcam initiated
-capture = cv2.VideoCapture(0)
-#Loop to capture images of employees frame by frame via a webcam which is of the device id 0 here
-print("\n[+] If you want to terminate the live feed, just press ctrl+c as the keyboard interrupt")
-while True:
-    ret, image = capture.read()
-    try:
-        #Resize image for faster processing
-        small_image = cv2.resize(image, (0,0), None, 0.20, 0.20)
-        #Find face locations in webcam feed
-        current_face = face_recognition.face_locations(small_image)
-        #Encode found faces
-        current_encoding = face_recognition.face_encodings(small_image, current_face)
-        #Loop to iterate through all the found faces and their encoding in current frame
-        for encoded_face, face_location in zip(current_encoding, current_face):
-            #Compare and match encoded faces from feed frame by frame to known encoded list of employee images
-            match = face_recognition.compare_faces(encoded_employee_list, encoded_face)
-            #Compare face distance values from live feed with values from encoded employee list
-            face_distance = face_recognition.face_distance(encoded_employee_list, encoded_face)
-            #Select the employee index for the employee with the highest similarity with face in current frame
-            match_index = np.argmin(face_distance)
-            #Condition to draw a rectangle and display the name of the found face in feed 
-            if match[match_index]:
-                name = employee_names[match_index].title()
-                top, right, bottom, left = face_location 
-                top*=5
-                bottom*=5
-                right*=5
-                left*=5
-                #Draw Rectangle on face 
-                cv2.rectangle(image, (left, top), (right, bottom), (0,0,255), 1)
-                cv2.rectangle(image, (left, bottom-15), (right, bottom), (0,0,255), cv2.FILLED)
-                #Put name on text
-                cv2.putText(image, name, (left+3, bottom-3), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 1)
-                #Mark the attendance of identified employee
-                attendance(name)
-        #Display live feed
-        cv2.imshow("Webcam Feed", image)
-        key=cv2.waitKey(1) & 0xFF
-        if key == ord('q'): 
-            break      
-    except:
-        #Error exception 
-        print("\n[-] Webcam not detected or the feed has been interrupted.")
-        print("[-] Terminating!")
-        sys.exit(1)
+    capture.release()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
